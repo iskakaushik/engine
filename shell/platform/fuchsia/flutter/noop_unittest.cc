c2c247b4dd0dff5aea913718d48d474f64158925
Kaushik Iska <kaushikiska@google.com>
2019-08-30 09:48:29 -0700

    tmp

--- shell/platform/fuchsia/flutter/noop_unittest.cc
+++ shell/platform/fuchsia/flutter/noop_unittest.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "gtest/gtest.h"
-
-TEST(NoopUnittest, TestThatWillAlwaysPass) {
-  EXPECT_TRUE(true);
+int main() {
+  return 0;
 }
