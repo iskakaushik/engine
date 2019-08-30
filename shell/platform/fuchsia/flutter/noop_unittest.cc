98557d43cc098d958a62c1fb5a15f964dc37bff8
Kaushik Iska <kaushikiska@google.com>
2019-08-30 09:49:58 -0700

    tmp

--- shell/platform/fuchsia/flutter/noop_unittest.cc
+++ shell/platform/fuchsia/flutter/noop_unittest.cc
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <iostream>
+
 int main() {
+  std::cout << "Successfully ran noop_unittest.cc" << std::endl;
   return 0;
 }
